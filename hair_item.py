e75b53bdd2908e1836fa8ad87dca351c11ffcb0d
seohyun-kim <selenium34@inu.ac.kr>
2022-10-17 03:44:14 +0900

    헤어 샘플 이미지 적용

--- hair_item.py
+++ hair_item.py
@@ -6,10 +6,11 @@ import imutils
 
 # 영상 및 돼지코 이미지 로드
 cap = cv2.VideoCapture(0)
-nose_img = cv2.imread('./img/hair.png')
+hair_img = cv2.imread('./img/hair3.png')
 
 # 5개의 center nose landmark point
-nose_landmarks = [49, 279, 197, 2, 5]
+hair_landmarks = [127, 356, 10, 9, 10]
+
 
 # mediapipe 호출
 mpFaceMesh = mp.solutions.face_mesh
@@ -26,12 +27,12 @@ while True:
     if results.multi_face_landmarks:
         for face_landmarks in results.multi_face_landmarks:
             # nose landmarks
-            left_nose_x = 0
-            left_nose_y = 0
-            right_nose_x = 0
-            right_nose_y = 0
-            center_nose_x = 0
-            center_nose_y = 0
+            left_hair_x = 0
+            left_hair_y = 0
+            right_hair_x = 0
+            right_hair_y = 0
+            center_hair_x = 0
+            center_hair_y = 0
 
             # 각 랜드마크의 정보 가져오기
             for id, lm in enumerate(face_landmarks.landmark):
@@ -41,41 +42,41 @@ while True:
                 x, y = int(lm.x * w), int(lm.y * h)
 
                 # 앞서 설정한 nose_landmark와 일치하는 랜드마크 넘버에 대해 x,y좌표 부여
-                if id == nose_landmarks[0]:
-                    left_nose_x, left_nose_y = x, y
-                if id == nose_landmarks[1]:
-                    right_nose_x, right_nose_y = x, y
-                if id == nose_landmarks[4]:
-                    center_nose_x, center_nose_y = x, y
+                if id == hair_landmarks[0]:
+                    left_hair_x, left_hair_y = x, y
+                if id == hair_landmarks[1]:
+                    right_hair_x, right_hair_y = x, y
+                if id == hair_landmarks[4]:
+                    center_hair_x, center_hair_y = x, y
 
-            # nose_width 계산
-            nose_width = int(hypot(left_nose_x - right_nose_x, left_nose_y - right_nose_y * 1.2))
-            nose_height = int(nose_width * 0.77)
+            # hair_width 계산
+            hair_width = int(hypot(left_hair_x - right_hair_x, left_hair_y - right_hair_y * 1.2))
+            hair_height = int(hair_width * 0.77)
 
             # nose_width와 nose_height가 0이 아닐 때 돼지코 이미지를 해당 크기에 맞게 resize
-            if (nose_width and nose_height) != 0:
-                pig_nose = cv2.resize(nose_img, (nose_width, nose_height))
+            if (hair_width and hair_height) != 0:
+                hair_sample = cv2.resize(hair_img, (hair_width, hair_height))
 
             # nose_area 구하기
-            top_left = (int(center_nose_x - nose_width / 2), int(center_nose_y - nose_height / 2))
-            bottom_right = (int(center_nose_x + nose_width / 2), int(center_nose_y + nose_height / 2))
+            top_left = (int(center_hair_x - hair_width / 2), int(center_hair_y - hair_height / 2))
+            bottom_right = (int(center_hair_x + hair_width / 2), int(center_hair_y + hair_height / 2))
 
             nose_area = frame[
-                        top_left[1]: top_left[1] + nose_height,
-                        top_left[0]: top_left[0] + nose_width
+                        top_left[1]: top_left[1] + hair_height,
+                        top_left[0]: top_left[0] + hair_width
                         ]
 
             # nose mask 생성
-            pig_nose_gray = cv2.cvtColor(pig_nose, cv2.COLOR_BGR2GRAY)
-            _, nose_mask = cv2.threshold(pig_nose_gray, 25, 255, cv2.THRESH_BINARY_INV)
+            hair_sample_gray = cv2.cvtColor(hair_sample, cv2.COLOR_BGR2GRAY)
+            _, nose_mask = cv2.threshold(hair_sample_gray, 25, 255, cv2.THRESH_BINARY_INV)
             no_nose = cv2.bitwise_and(nose_area, nose_area, mask=nose_mask)
 
             # no_nose에 pig nose 중첩
-            final_nose = cv2.add(no_nose, pig_nose)
+            final_nose = cv2.add(no_nose, hair_sample)
             # pig nose filter를 영상에 적용
             frame[
-            top_left[1]: top_left[1] + nose_height,
-            top_left[0]: top_left[0] + nose_width
+            top_left[1]: top_left[1] + hair_height,
+            top_left[0]: top_left[0] + hair_width
             ] = final_nose
 
     # 변경된 이미지 출력
